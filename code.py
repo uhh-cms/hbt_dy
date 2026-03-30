85fc66773978d39fc26eca7ca49703f8753e883e
Vincent Langlo <vincent.a.langlo@gmail.com>
2026-03-30 17:39:41 +0200

    added two python files for testing plots and histograms

---
+++ code.py
@@ -0,0 +1,13 @@
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+print("hello world")
+for i in range(10):
+ print(i)
+
+List=[123456]
+Y=[135246]
+plt.plot(List,Y)
+plt.show()
+                
