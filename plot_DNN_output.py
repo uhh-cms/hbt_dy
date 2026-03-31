7c6796598db074743359264aefb141f293183ab4
Vincent Langlo <vincent.a.langlo@gmail.com>
2026-03-31 15:21:08 +0200

    Plot nach channel-ID unterteilt

--- plot_DNN_output.py
+++ plot_DNN_output.py
@@ -14,17 +14,33 @@ tt = Hist(hist.axis.Regular(bins=100, start=0, stop=1, name="x"))
 hh = Hist(hist.axis.Regular(bins=100, start=0, stop=1, name="x"))
 
 
-dy.fill(events_dy.run3_dnn_moe_hh,weight=events_dy.event_weight)
-tt.fill(events_tt.run3_dnn_moe_hh,weight=events_tt.event_weight)
-hh.fill(events_hh.run3_dnn_moe_hh,weight=events_hh.event_weight)
+#masken für channel-ID erstellen:
+id_1dy = (events_dy.channel_id == 1)
+id_2dy = (events_dy.channel_id == 2)
+id_3dy = (events_dy.channel_id == 3)
 
-plt.yscale('log')    #Achse logarithmisch skalieren 
+id_1tt = (events_tt.channel_id == 1)
+id_2tt = (events_tt.channel_id == 2)
+id_3tt = (events_tt.channel_id == 3)
 
-dy.plot(label="dy")
-tt.plot(label="tt")
-hh.plot(label="hh")
+id_1hh = (events_hh.channel_id == 1)
+id_2hh = (events_hh.channel_id == 2)
+id_3hh = (events_hh.channel_id == 3)
 
-plt.legend()
-plt.ylabel("number of events (weighted)")
-plt.title("DNN outputnode hh for dy,tt and hh simulatioins")
-plt.savefig("plots/hist_hhnode.png", dpi=300, bbox_inches='tight')
+
+for i in [1,2,3]:
+    dy.fill(events_dy.run3_dnn_moe_hh[events_dy.channel_id == i],weight=events_dy.event_weight[events_dy.channel_id == i])
+    tt.fill(events_tt.run3_dnn_moe_hh[events_tt.channel_id == i],weight=events_tt.event_weight[events_tt.channel_id == i])
+    hh.fill(events_hh.run3_dnn_moe_hh[events_hh.channel_id == i],weight=events_hh.event_weight[events_hh.channel_id == i])
+
+    plt.yscale('log')    #Achse logarithmisch skalieren 
+
+    dy.plot(label="dy")
+    tt.plot(label="tt")
+    hh.plot(label="hh")
+
+    plt.legend()
+    plt.ylabel("number of events (weighted)")
+    plt.title(f"DNN outputnode hh for dy,tt and hh simulatioins - channel{i}")
+    plt.savefig(f"plots/hist_hhnode_channel{i}.png", dpi=300, bbox_inches='tight')
+    plt.figure()
