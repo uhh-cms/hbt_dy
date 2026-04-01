2fe4f5ea3f5fb3e5d8837de932870af52e8fa408
Vincent Langlo <vincent.a.langlo@gmail.com>
2026-04-01 16:53:13 +0200

    Fehler gefunden: Histogramme werden jetzt resetted; außerdem: Histogramme mit tt im Stack hinzugefügt

--- plot_DNN_output.py
+++ plot_DNN_output.py
@@ -20,11 +20,11 @@ hh = Hist(hist.axis.Regular(bins=100, start=0, stop=1, name="x"))
 channelname=["e-tau", "mu-tau", "tau-tau"]
 
 
-#Histogramme nach channel aufteilen, fillen (für dy nach Zerfallskanal aufteilen + stacken), plotten.
+#1. Histogramme nach channel aufteilen, fillen (für dy nach Zerfallskanal aufteilen + stacken), plotten.
 for i in [1,2,3]:
-    dy.fill(x=events_dy.run3_dnn_moe_hh[(events_dy.channel_id == i) & (events_dy.gen_ll_pdgid == 11)],Zerfallskanal="e", weight=events_dy.event_weight[(events_dy.channel_id == i) & (events_dy.gen_ll_pdgid == 11)])    #maske für channel (und bei dy Zerfallskanal) in eckigen Klammern
-    dy.fill(x=events_dy.run3_dnn_moe_hh[(events_dy.channel_id == i) & (events_dy.gen_ll_pdgid == 13)],Zerfallskanal="mu", weight=events_dy.event_weight[(events_dy.channel_id == i) & (events_dy.gen_ll_pdgid == 13)])
-    dy.fill(x=events_dy.run3_dnn_moe_hh[(events_dy.channel_id == i) & (events_dy.gen_ll_pdgid == 15)],Zerfallskanal="tau", weight=events_dy.event_weight[(events_dy.channel_id == i) & (events_dy.gen_ll_pdgid == 15)])
+    dy.fill(x=events_dy.run3_dnn_moe_hh[(events_dy.channel_id == i) & (events_dy.gen_ll_pdgid == 11)],Zerfallskanal=r"gen: DY $\to e^+e^-$", weight=events_dy.event_weight[(events_dy.channel_id == i) & (events_dy.gen_ll_pdgid == 11)])    #maske für channel (und bei dy Zerfallskanal) in eckigen Klammern
+    dy.fill(x=events_dy.run3_dnn_moe_hh[(events_dy.channel_id == i) & (events_dy.gen_ll_pdgid == 13)],Zerfallskanal=r"gen: DY $\to \mu^+\mu^-$", weight=events_dy.event_weight[(events_dy.channel_id == i) & (events_dy.gen_ll_pdgid == 13)])
+    dy.fill(x=events_dy.run3_dnn_moe_hh[(events_dy.channel_id == i) & (events_dy.gen_ll_pdgid == 15)],Zerfallskanal=r"gen: DY $\to \tau^+\tau^-$", weight=events_dy.event_weight[(events_dy.channel_id == i) & (events_dy.gen_ll_pdgid == 15)])
 
     tt.fill(events_tt.run3_dnn_moe_hh[events_tt.channel_id == i],weight=events_tt.event_weight[events_tt.channel_id == i])
     hh.fill(events_hh.run3_dnn_moe_hh[events_hh.channel_id == i],weight=events_hh.event_weight[events_hh.channel_id == i])
@@ -35,43 +35,86 @@ for i in [1,2,3]:
     stack = dy.stack("Zerfallskanal")
     stack.plot(stack=True, histtype="fill") # 'stack=True' ist entscheidend!
 
-    tt.plot(label="tt")
-    hh.plot(label="hh")
+    tt.plot(label=r"$t\bar{t}$")
+    hh.plot(label=r"$HH$")
 
     plt.legend()
     plt.ylabel("number of events (weighted)")
-    plt.title(f"DNN outputnode hh for dy,tt and hh simulatioins - {channelname[i-1]}-channel")
+    plt.xlabel("Di-Higgs-outputnode of the DNN")
+    plt.title(f"Histogram of DNN-outputnode $HH$ for dy,tt and hh simulatioins - {channelname[i-1]}-channel")
     plt.savefig(f"plots/hist_hhnode/{channelname[i-1]}-channel.png", dpi=300, bbox_inches='tight')
     plt.figure()
 
+    #histogramme für nächste iteration clearen
+    dy.reset()
+    tt.reset()
+    hh.reset()
 
 
-#Das gleiche normiert in linearer skala plotten, linearisierung und normierung fehlt noch
+#2. Normierte, lineare histogramme plotten
 for i in [1,2,3]:
-    dy.fill(x=events_dy.run3_dnn_moe_hh[(events_dy.channel_id == i) & (events_dy.gen_ll_pdgid == 11)],Zerfallskanal="e", weight=events_dy.event_weight[(events_dy.channel_id == i) & (events_dy.gen_ll_pdgid == 11)])    #maske für channel (und bei dy Zerfallskanal) in eckigen Klammern
-    dy.fill(x=events_dy.run3_dnn_moe_hh[(events_dy.channel_id == i) & (events_dy.gen_ll_pdgid == 13)],Zerfallskanal="mu", weight=events_dy.event_weight[(events_dy.channel_id == i) & (events_dy.gen_ll_pdgid == 13)])
-    dy.fill(x=events_dy.run3_dnn_moe_hh[(events_dy.channel_id == i) & (events_dy.gen_ll_pdgid == 15)],Zerfallskanal="tau", weight=events_dy.event_weight[(events_dy.channel_id == i) & (events_dy.gen_ll_pdgid == 15)])
+    dy.fill(x=events_dy.run3_dnn_moe_hh[(events_dy.channel_id == i) & (events_dy.gen_ll_pdgid == 11)],Zerfallskanal=r"gen: DY $\to e^+e^-$", weight=events_dy.event_weight[(events_dy.channel_id == i) & (events_dy.gen_ll_pdgid == 11)])    #maske für channel (und bei dy Zerfallskanal) in eckigen Klammern
+    dy.fill(x=events_dy.run3_dnn_moe_hh[(events_dy.channel_id == i) & (events_dy.gen_ll_pdgid == 13)],Zerfallskanal=r"gen: DY $\to \mu^+\mu^-$", weight=events_dy.event_weight[(events_dy.channel_id == i) & (events_dy.gen_ll_pdgid == 13)])
+    dy.fill(x=events_dy.run3_dnn_moe_hh[(events_dy.channel_id == i) & (events_dy.gen_ll_pdgid == 15)],Zerfallskanal=r"gen: DY $\to \tau^+\tau^-$", weight=events_dy.event_weight[(events_dy.channel_id == i) & (events_dy.gen_ll_pdgid == 15)])
 
     tt.fill(events_tt.run3_dnn_moe_hh[events_tt.channel_id == i],weight=events_tt.event_weight[events_tt.channel_id == i])
     hh.fill(events_hh.run3_dnn_moe_hh[events_hh.channel_id == i],weight=events_hh.event_weight[events_hh.channel_id == i])
 
-    #plt.yscale('log')    #Achse logarithmisch skalieren 
 
     # Stack-Plot erstellen
     stack = dy.stack("Zerfallskanal")
     stack.plot(stack=True, histtype="fill", density=True) # 'stack=True' ist entscheidend!
 
-    tt.plot(label="tt", density=True)
-    hh.plot(label="hh", density=True)
+    tt.plot(label=r"$t\bar{t}$", density=True)
+    hh.plot(label=r"$HH$", density=True)
 
     plt.legend()
     plt.ylabel("number of events (normalized)")
-    plt.title(f"DNN outputnode hh for dy,tt and hh simulatioins - {channelname[i-1]}-channel")
+    plt.xlabel("Di-Higgs-outputnode of the DNN")
+    plt.title(f"Histogram of DNN-outputnode $HH$ for dy,tt and hh simulatioins - {channelname[i-1]}-channel")
     plt.savefig(f"plots/hist_hhnode_lin/{channelname[i-1]}-channel_linear.png", dpi=300, bbox_inches='tight')
     plt.figure()
+
+    #histogramme für nächste iteration clearen
+    dy.reset()
+    tt.reset()
+    hh.reset()
+
+
+#3. wie 1, aber tt wird zum stack hinzugefügt
+for i in [1,2,3]:
+    dy.fill(x=events_dy.run3_dnn_moe_hh[(events_dy.channel_id == i) & (events_dy.gen_ll_pdgid == 11)],Zerfallskanal=r"gen: DY $\to e^+e^-$", weight=events_dy.event_weight[(events_dy.channel_id == i) & (events_dy.gen_ll_pdgid == 11)])    #maske für channel (und bei dy Zerfallskanal) in eckigen Klammern
+    dy.fill(x=events_dy.run3_dnn_moe_hh[(events_dy.channel_id == i) & (events_dy.gen_ll_pdgid == 13)],Zerfallskanal=r"gen: DY $\to \mu^+\mu^-$", weight=events_dy.event_weight[(events_dy.channel_id == i) & (events_dy.gen_ll_pdgid == 13)])
+    dy.fill(x=events_dy.run3_dnn_moe_hh[(events_dy.channel_id == i) & (events_dy.gen_ll_pdgid == 15)],Zerfallskanal=r"gen: DY $\to \tau^+\tau^-$", weight=events_dy.event_weight[(events_dy.channel_id == i) & (events_dy.gen_ll_pdgid == 15)])
+    dy.fill(x=events_tt.run3_dnn_moe_hh[events_tt.channel_id == i],Zerfallskanal=r"$t\bar{t}$", weight=events_tt.event_weight[events_tt.channel_id == i])
+
+    tt.fill(events_tt.run3_dnn_moe_hh[events_tt.channel_id == i],weight=events_tt.event_weight[events_tt.channel_id == i])
+    hh.fill(events_hh.run3_dnn_moe_hh[events_hh.channel_id == i],weight=events_hh.event_weight[events_hh.channel_id == i])
+
+    plt.yscale('log')    #Achse logarithmisch skalieren 
+
+    # Stack-Plot erstellen
+    stack = dy.stack("Zerfallskanal") #technically zerfallskanal+tt als korrekter name
+    stack.plot(stack=True, histtype="fill") # 'stack=True' ist entscheidend!
+
+    tt.plot(label=r"$t\bar{t}$")
+    hh.plot(label=r"$HH$")
+
+    plt.legend()
+    plt.ylabel("number of events (weighted)")
+    plt.xlabel("Di-Higgs-outputnode of the DNN")
+    plt.title(f"Histogram of DNN-outputnode $HH$ for dy,tt and hh simulatioins - {channelname[i-1]}-channel")
+    plt.savefig(f"plots/hist_hhnode_stacked-tt/{channelname[i-1]}-channel.png", dpi=300, bbox_inches='tight')
+    plt.figure()
+
+    #histogramme für nächste iteration clearen
+    dy.reset()
+    tt.reset()
+    hh.reset()
 
 
 
 #channel zuordnungen 1: e tau, 2: mu tau, 3: tau tau
 
-#to do: Legende sehr ausführlich machen (gen: dy->tt, etc), tt zum Stack hinzufügen, migration plot von dy type zu den channels, x achse labeln
+#to do: migration plot von dy type zu den channels
+
