ad4796f535ce5f2eb2b8a844ebd4803ed5dbc634
Vincent Langlo <vincent.a.langlo@gmail.com>
2026-04-27 16:22:59 +0200

    Signifikanz zu den Plots aus hist_1.py hinzugefügt

--- aktuell.py
+++ aktuell.py
@@ -1,7 +1,40 @@
-thisdict={}
-thisdict["color"]="red"
-print(thisdict)
+#1. Histogramme nach channel aufteilen, fillen (für dy nach Zerfallskanal aufteilen + stacken), plotten.
+for i in [1,2,3]:
+    dy.fill(x=events_dy.run3_dnn_moe_hh[(events_dy.channel_id == i) & (events_dy.gen_ll_pdgid == 11)],Zerfallskanal=r"gen: DY $\to e^+e^-$", weight=events_dy.event_weight[(events_dy.channel_id == i) & (events_dy.gen_ll_pdgid == 11)])    #maske für channel (und bei dy Zerfallskanal) in eckigen Klammern
+    dy.fill(x=events_dy.run3_dnn_moe_hh[(events_dy.channel_id == i) & (events_dy.gen_ll_pdgid == 13)],Zerfallskanal=r"gen: DY $\to \mu^+\mu^-$", weight=events_dy.event_weight[(events_dy.channel_id == i) & (events_dy.gen_ll_pdgid == 13)])
+    dy.fill(x=events_dy.run3_dnn_moe_hh[(events_dy.channel_id == i) & (events_dy.gen_ll_pdgid == 15)],Zerfallskanal=r"gen: DY $\to \tau^+\tau^-$", weight=events_dy.event_weight[(events_dy.channel_id == i) & (events_dy.gen_ll_pdgid == 15)])
 
-thisdict.update({"year":"1991"})
-print(thisdict)
-print(thisdict["color"])
+    tt.fill(events_tt.run3_dnn_moe_hh[events_tt.channel_id == i],weight=events_tt.event_weight[events_tt.channel_id == i])
+    hh.fill(events_hh.run3_dnn_moe_hh[events_hh.channel_id == i],weight=events_hh.event_weight[events_hh.channel_id == i])
+
+    #from IPython import embed; embed(header="MESSAGE Line 34 | File: hists_1.py")
+    fig, ax1 = plt.subplots()
+    background_bins = np.sum(dy.values(),axis=0)+ tt.values()
+    signal_bins = hh.values()
+    significance = signal_bins**2/background_bins
+    ax2 = ax1.twinx()  # Erstellt die rechte Achse
+    ax2.plot(np.linspace(0, 1, 100),significance, label="significance")
+    ax2.set_ylabel('Exponentielle Werte')
+    ax2.tick_params(axis='y', labelcolor='b')
+    #plt.plot(significance)
+
+    plt.yscale('log')    #Achse logarithmisch skalieren 
+
+    # Stack-Plot erstellen
+    stack = dy.stack("Zerfallskanal")
+    stack.plot(stack=True, histtype="fill") # 'stack=True' ist entscheidend!
+
+    tt.plot(label=r"$t\bar{t}$")
+    hh.plot(label=r"$HH$")
+
+    plt.legend()
+    plt.ylabel("number of events (weighted)")
+    plt.xlabel("Di-Higgs-outputnode of the DNN")
+    plt.title(f"Histogram of DNN-outputnode $HH$ for dy,tt and hh simulatioins - {channelname_r[i-1]}-channel")
+    plt.savefig(f"plots/hist_hhnode/{channelname[i-1]}-channel.png", dpi=300, bbox_inches='tight')
+    plt.figure()
+
+    #histogramme für nächste iteration clearen
+    dy.reset()
+    tt.reset()
+    hh.reset()
--- hists_1.py
+++ hists_1.py
@@ -31,28 +31,38 @@ for i in [1,2,3]:
     tt.fill(events_tt.run3_dnn_moe_hh[events_tt.channel_id == i],weight=events_tt.event_weight[events_tt.channel_id == i])
     hh.fill(events_hh.run3_dnn_moe_hh[events_hh.channel_id == i],weight=events_hh.event_weight[events_hh.channel_id == i])
 
-    #from IPython import embed; embed(header="MESSAGE Line 34 | File: hists_1.py")
-    fig, ax1 = plt.subplots()
-    background_bins = np.sum(dy.values(),axis=0)+ tt.values()
-    signal_bins = hh.values()
-    significance = signal_bins**2/background_bins
-    ax2 = ax1.twinx()  # Erstellt die rechte Achse
-    ax2.plot(np.linspace(0, 1, 100),significance, label="significance")
-    ax2.set_ylabel('Exponentielle Werte')
-    ax2.tick_params(axis='y', labelcolor='b')
-    #plt.plot(significance)
 
-    plt.yscale('log')    #Achse logarithmisch skalieren 
+    fig, ax1 = plt.subplots()
 
     # Stack-Plot erstellen
     stack = dy.stack("Zerfallskanal")
-    stack.plot(stack=True, histtype="fill") # 'stack=True' ist entscheidend!
+    stack.plot(stack=True, histtype="fill",ax=ax1) # 'stack=True' ist entscheidend!
 
-    tt.plot(label=r"$t\bar{t}$")
-    hh.plot(label=r"$HH$")
+    tt.plot(label=r"$t\bar{t}$",ax=ax1)
+    hh.plot(label=r"$HH$",ax=ax1)
 
-    plt.legend()
-    plt.ylabel("number of events (weighted)")
+    ax1.set_ylabel("number of events (weighted)")
+    plt.yscale('log')    #linke Achse logarithmisch skalieren 
+
+    #zweite Achse
+    background_bins = np.sum(dy.values(),axis=0)+ tt.values()
+    signal_bins = hh.values()
+    significance = signal_bins**2/background_bins
+    significance_total = round(np.sum(significance),3)
+    ax2 = ax1.twinx()  # Erstellt die rechte Achse
+    ax2.step(np.linspace(0, 1, 100),significance, label=f"significance (total = {significance_total})", color="black")
+    ax2.set_ylabel('Significance')
+    ax2.tick_params(axis='y', labelcolor='black')
+
+    plt.yscale('log')    #rechte Achse logarithmisch skalieren 
+
+    #legende:
+    h1, l1 = ax1.get_legend_handles_labels()
+    h2, l2 = ax2.get_legend_handles_labels()
+    # Gemeinsam plotten
+    ax1.legend(h1 + h2, l1 + l2, loc="upper center", ncol=2, frameon=True)
+
+
     plt.xlabel("Di-Higgs-outputnode of the DNN")
     plt.title(f"Histogram of DNN-outputnode $HH$ for dy,tt and hh simulatioins - {channelname_r[i-1]}-channel")
     plt.savefig(f"plots/hist_hhnode/{channelname[i-1]}-channel.png", dpi=300, bbox_inches='tight')
@@ -65,36 +75,6 @@ for i in [1,2,3]:
 
 
 
-#2. Normierte, lineare histogramme plotten (recht unwichtig)
-for i in [1,2,3]:
-    dy.fill(x=events_dy.run3_dnn_moe_hh[(events_dy.channel_id == i) & (events_dy.gen_ll_pdgid == 11)],Zerfallskanal=r"gen: DY $\to e^+e^-$", weight=events_dy.event_weight[(events_dy.channel_id == i) & (events_dy.gen_ll_pdgid == 11)])    #maske für channel (und bei dy Zerfallskanal) in eckigen Klammern
-    dy.fill(x=events_dy.run3_dnn_moe_hh[(events_dy.channel_id == i) & (events_dy.gen_ll_pdgid == 13)],Zerfallskanal=r"gen: DY $\to \mu^+\mu^-$", weight=events_dy.event_weight[(events_dy.channel_id == i) & (events_dy.gen_ll_pdgid == 13)])
-    dy.fill(x=events_dy.run3_dnn_moe_hh[(events_dy.channel_id == i) & (events_dy.gen_ll_pdgid == 15)],Zerfallskanal=r"gen: DY $\to \tau^+\tau^-$", weight=events_dy.event_weight[(events_dy.channel_id == i) & (events_dy.gen_ll_pdgid == 15)])
-
-    tt.fill(events_tt.run3_dnn_moe_hh[events_tt.channel_id == i],weight=events_tt.event_weight[events_tt.channel_id == i])
-    hh.fill(events_hh.run3_dnn_moe_hh[events_hh.channel_id == i],weight=events_hh.event_weight[events_hh.channel_id == i])
-
-
-    # Stack-Plot erstellen
-    stack = dy.stack("Zerfallskanal")
-    stack.plot(stack=True, histtype="fill", density=True) # 'stack=True' ist entscheidend!
-
-    tt.plot(label=r"$t\bar{t}$", density=True)
-    hh.plot(label=r"$HH$", density=True)
-
-    plt.legend()
-    plt.ylabel("number of events (normalized)")
-    plt.xlabel("Di-Higgs-outputnode of the DNN")
-    plt.title(f"Histogram of DNN-outputnode $HH$ for dy,tt and hh simulatioins - {channelname_r[i-1]}-channel")
-    plt.savefig(f"plots/hist_hhnode_lin/{channelname[i-1]}-channel_linear.png", dpi=300, bbox_inches='tight')
-    plt.figure()
-
-    #histogramme für nächste iteration clearen
-    dy.reset()
-    tt.reset()
-    hh.reset()
-
-
 
 #3. wie 1, aber tt wird zum stack hinzugefügt
 for i in [1,2,3]: #i steht für den channel
@@ -105,16 +85,40 @@ for i in [1,2,3]: #i steht für den channel
 
     hh.fill(events_hh.run3_dnn_moe_hh[events_hh.channel_id == i],weight=events_hh.event_weight[events_hh.channel_id == i])
 
-    plt.yscale('log')    #Achse logarithmisch skalieren 
+    fig, ax1 = plt.subplots()
 
     # Stack-Plot erstellen
     stack = dy.stack("Zerfallskanal") #technically zerfallskanal+tt als korrekter name
-    stack.plot(stack=True, histtype="fill") # 'stack=True' ist entscheidend!
+    stack.plot(stack=True, histtype="fill", ax=ax1) # 'stack=True' ist entscheidend!
 
-    hh.plot(label=r"$HH$")
+    hh.plot(label=r"$HH$",ax=ax1)
+
+    ax1.set_ylabel("number of events (weighted)")
+    plt.yscale('log')    #linke Achse logarithmisch skalieren 
+
+    from IPython import embed; embed(header="MESSAGE Line 99 | File: hists_1.py")
+
+    #zweite Achse
+    background_bins = np.sum(dy.values(),axis=0)
+    signal_bins = hh.values()
+    significance = signal_bins**2/background_bins
+    significance_total = round(np.sum(significance),3)
+    ax2 = ax1.twinx()  # Erstellt die rechte Achse
+    ax2.step(np.linspace(0, 1, 100),significance, label=f"significance (total = {significance_total})", color="black")
+    ax2.set_ylabel('Significance')
+    ax2.tick_params(axis='y', labelcolor='black')
+
+    plt.yscale('log')    #rechte Achse logarithmisch skalieren 
+
+    #legende:
+    h1, l1 = ax1.get_legend_handles_labels()
+    h2, l2 = ax2.get_legend_handles_labels()
+    # Gemeinsam plotten
+    ax1.legend(h1 + h2, l1 + l2, loc="upper center", ncol=2, frameon=True)
+
+
 
     plt.legend()
-    plt.ylabel("number of events (weighted)")
     plt.xlabel("Di-Higgs-outputnode of the DNN")
     plt.title(f"Histogram of DNN-outputnode $HH$ for dy,tt and hh simulatioins - {channelname_r[i-1]}-channel")
     plt.savefig(f"plots/hist_hhnode_stacked-tt/{channelname[i-1]}-channel.png", dpi=300, bbox_inches='tight')
