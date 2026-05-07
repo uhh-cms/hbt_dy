eb6434a084ff6d65b8547040fbcddffb0449819a
Vincent Langlo <vincent.a.langlo@gmail.com>
2026-05-07 14:19:05 +0200

    zu origin_of_reco-tau.py noch Unterteilung in cat_id hinzugefügt

--- plotting/origin_of_reco-tau.py
+++ plotting/origin_of_reco-tau.py
@@ -153,3 +153,43 @@ plt.grid(axis='y', linestyle='--', alpha=0.7)
 
 plt.savefig("plots/bar-charts/origin-reco-tau_no_cut_no_weights.png", dpi=300, bbox_inches='tight')
 plt.figure()
+
+
+#====================================================
+#2.1 bar chart for seeing origin of reconstructed tau_h with category id
+#====================================================
+
+#maske definieren
+hh_cut = 0.9
+dy_mask = events_dy.run3_dnn_moe_hh > hh_cut
+
+
+for i,id in enumerate([147,151,175,179,203,207],start=0): #id steht für category ids, i ist index
+    id_mask = ak.any(events_dy.category_ids == id,axis=1)
+
+    #weights definieren
+    weights_ak = ak.zeros_like(events_dy.tau_genPartFlav) + events_dy.event_weight
+    weights = ak.flatten(weights_ak[dy_mask & id_mask])
+
+    #alle category_ids zusammen nicht alle events glaub ich...
+
+    flat_data = ak.flatten(events_dy.tau_genPartFlav[(dy_mask) & (id_mask)])
+    np_data = ak.to_numpy(flat_data)
+    counts = np.bincount(np_data, weights = weights) #minlength=6 ?
+    x_labels = np.arange(6)
+
+    plt.figure()
+    plt.bar(x_labels, counts, color='#4C72B0', edgecolor='black')
+
+    plt.xlabel('Origins', fontsize=12)
+    plt.ylabel('Number of events', fontsize=12)
+    plt.title(fr'Origins of reconstructed $\tau_h$ (cat_id: {id}, ${hh_cut}$ cut)', fontsize=14)
+
+    # Sicherstellen, dass nur ganze Zahlen auf der X-Achse stehen
+    plt.xticks(x_labels,labels)
+
+    # Raster im Hintergrund für bessere Lesbarkeit
+    plt.grid(axis='y', linestyle='--', alpha=0.7)
+
+    plt.savefig(f"plots/bar-charts/cat_id/origin-reco-tau_{id}.png", dpi=300, bbox_inches='tight')
+    plt.figure()
